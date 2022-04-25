4202c9d861c23eaeb9c3a979942513e13676cbb4
Sami Taaissat <sam.taa@icloud.com>
2022-04-25 22:56:04 +0200

    update

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,7 @@
 const YEAR = new Date().getFullYear()
 
 export default {
+titleSuffix: ' – Kakise\'s blog',
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Kakise.
